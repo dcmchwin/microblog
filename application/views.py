948da7f1c5336546b191c83672f677853c69e067
David McHardy <dcmchwin@gmail.com>
2017-05-11 21:44:37 +0100

    as per previous

--- application/views.py
+++ application/views.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, world!"
+@app.route('/<user>')
+def index(user=None):
+    title = 'INDEX'
+    if user is None:
+        kwargs = dict(title=title)
+    else:
+        kwargs = dict(title=title, user=user)
+    return render_template('index.html', **kwargs)
